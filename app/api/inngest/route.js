dd8c4250a14086fe7afcc2ff4f7b61f2e9cd7396
July Franz Claridad <claridadjulyfranz@gmail.com>
2025-06-28 00:47:23 +0800

    Fourth Commit

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,6 +1,6 @@
 import { serve } from "inngest/next";
-import { inngest } from "../config/inngest";
-import { syncUserCreation, syncUserDeletion, syncUserUpdate } from "@/config/inngest";
+import { inngest } from "../../config/inngest";
+import { syncUserCreation, syncUserDeletion, syncUserUpdate } from "../../config/inngest";
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
