d94fad1b4739b0754c43dbf0692d9ff85d7d8243
July Franz Claridad <claridadjulyfranz@gmail.com>
2025-06-28 00:37:11 +0800

    second commit

--- context/AppContext.jsx
+++ context/AppContext.jsx
@@ -15,6 +15,7 @@ export const AppContextProvider = (props) => {
     const currency = process.env.NEXT_PUBLIC_CURRENCY
     const router = useRouter()
 
+
     const { user } = useUser()
 
     const [products, setProducts] = useState([])
